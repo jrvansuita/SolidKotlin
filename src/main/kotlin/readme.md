4577eac3395a1645e5b52014fd4a6c50339b63a6
juniorvansuita <Vansuita1324>
2022-07-20 22:59:42 -0300

    Initial commit

---
+++ src/main/kotlin/readme.md
@@ -0,0 +1,10 @@
+# Solid Principles using Kotlin
+
+Just a study about SOLID principles using Kotlin. 
+Here are some classes implementing every SOLID principle.
+
+- **S**ingle-responsibility principle ```SingleResponsibility.kt```
+- **O**pen–closed principle
+- **L**iskov substitution principle
+- **I**nterface segregation principle
+- **D**ependency inversion principle
---
+++ src/main/kotlin/s/SingleResponsibility.kt
@@ -0,0 +1,52 @@
+package s
+
+import kotlin.random.Random
+
+/**
+ * Single-Responsibility Principle
+ * A class should have only one reason to exists and responsible for one thing.
+ */
+class SingleResponsibility {
+
+    class User(val id: Int, val name: String, val email: String)
+
+    /* ❌ Violation Example  */
+    //This class handles way too much
+
+    class SessionHandler {
+        fun startSession(user: User) {
+            println("Starting ${user.name} session with ${user.id} id.")
+        }
+
+        fun activateUserAccount(user: User) {
+            println("Activating ${user.name}.")
+        }
+
+        fun checkAccessRights(user: User) {
+            println("Checking ${user.name} access.")
+        }
+    }
+
+
+    /* ✅ Correct Example */
+   //Now we can clearly see that each class is responsible for exactly one thing
+
+    class Session {
+        fun start(user: User) {
+            println("Starting ${user.name} session with ${user.id} id.")
+        }
+    }
+
+    class UserAccount {
+        fun activate(user: User) {
+            println("Activating ${user.name}.")
+        }
+    }
+
+    class AccessRights {
+        fun check(user: User) {
+            println("Checking ${user.name} access.")
+        }
+    }
+
+}
