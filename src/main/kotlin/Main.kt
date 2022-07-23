6ccf812b48700135c7d031fff3e271ccfbfdb3ce
juniorvansuita <Vansuita1324>
2022-07-23 15:35:30 -0300

    main call

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array<String>) {
     println("Solid Study")
 
-
-
-
+    SingleResponsibility()
+    OpenClose()
+    LiskovSubstitution()
+    InterfaceSegregation()
+    DependencyInversion()
 }
